339674600033f8da6006dfc7689910d8d99189a0
mbdullah <mdabdullah162005@gmail.com>
2025-07-12 14:33:47 +0600

    included add properties api and save property data in db

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jsryxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jsryxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,13 +23,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     const usersCollection = client.db("real_estate_DB").collection("users");
+    const agentPropertiesCollection = client.db("real_estate_DB").collection("properties");
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     // * USERS COllections Related API
+
+    // Role based api
+    app.get("/users/:email/role", async (req, res) => {
+      try {
+        const email = req.params.email;
+
+        if (!email) {
+          return res.status(400).send({ message: "email is required" });
+        }
+        const user = await usersCollection.findOne({ email });
+
+        if (!user) {
+          return res
+            .status(404)
+            .send({ message: "User not found", role: null });
+        }
+        res.send({ role: user.role || "user" });
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -42,8 +63,14 @@ async function run() {
       res.send(result);
     });
 
+    // * AGENT COLLECTION RELATED API
+    // Add Property API
+    app.post("/properties", async (req, res) => {
+      const property = req.body;
+      const result = await agentPropertiesCollection.insertOne(property);
+      res.send(result);
+    });
 
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
